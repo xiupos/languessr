3d63aa48a0d9d6c38aed59311a01a85ea43e4240
xiupos <me@xiupos.net>
2023-09-25 14:22:34 +0900

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "LangGuessr",
+        short_name: "LangGuessr",
+        description:
+          "LangGuessr is a language game which shows you summaries of Wikipedia articles and challenges your ability to recognize languages.",
+        theme_color: "#1095c1",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
